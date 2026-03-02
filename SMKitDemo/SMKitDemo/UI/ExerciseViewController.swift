c5ff1b9e9a66fb900afa63fb91974ea74194e469
Offry Hayon <offry.h@sency.ai>
2026-03-02 08:34:10 +0200

    Update to align with release smkit 1.5.1

--- SMKitDemo/SMKitDemo/UI/ExerciseViewController.swift
+++ SMKitDemo/SMKitDemo/UI/ExerciseViewController.swift
@@ -183,13 +183,12 @@ extension ExerciseViewController:SMKitSessionDelegate{
         }
     }
     
-    func handlePositionData(poseData2D: [Joint : JointData]?, poseData3D: [Joint : SCNVector3]?, jointAnglesData: [LimbsPairs : Float]?, jointGlobalAnglesData: [Limbs : Float]?, xyzEulerAngles: [String : SCNVector3]?) {
+    func handlePositionData(poseData2D: [Joint : JointData]?, poseData3D: [Joint : SCNVector3]?, jointAnglesData: [LimbsPairs : Float]?, jointGlobalAnglesData: [Limbs : Float]?, xyzEulerAngles: [String : SCNVector3]?, xyzRelativeAngles: [String : SCNVector3]?) {
         DispatchQueue.main.async {[weak self] in
             guard let self, let previewLayer else {return}
             let captureSize = previewLayer.frame.size
             let videoResultion = (previewLayer.session?.sessionPreset ?? .hd1920x1080).videoSize
-            // Todo: resolve that in spm 1.4.6 xcode 26.2
-//            skeletonView.updateSkeleton(rawData: poseData2D ?? [:], captureSize: captureSize, videoSize: videoResultion)
+            skeletonView.updateSkeleton(rawData: poseData2D ?? [:], captureSize: captureSize, videoSize: videoResultion)
         }
     }
     
